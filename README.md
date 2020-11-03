64db7179bf5eb6999c7e058d76b8392a0962f3b7
Aaron Wrasman <awrasman@enchanted.net>
2020-11-02 20:13:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# intune-miner
+
+Scrape Nessus cloud scanner addresses from https://docs.microsoft.com/en-us/mem/intune/fundamentals/intune-endpoints
+
+## Installation
+
+Install through *System->Extensions->git*
+
+
---
+++ intune/node.py
@@ -0,0 +1,60 @@
+import logging
+import json
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+from minemeld.ft.basepoller import BasePollerFT
+
+LOG = logging.getLogger(__name__)
+
+
+class Miner(BasePollerFT):
+    def configure(self):
+        super(Miner, self).configure()
+
+        self.polling_timeout = self.config.get('polling_timeout', 30)
+        self.verify_cert = self.config.get('verify_cert', True)
+        self.url = 'https://docs.microsoft.com/en-us/mem/intune/fundamentals/intune-endpoints'
+
+    def _build_iterator(self, item):
+        # builds the request and retrieves the page
+        rkwargs = dict(
+            stream=False,
+            verify=self.verify_cert,
+            timeout=self.polling_timeout,
+        )
+
+        r = requests.get(
+            self.url,
+            **rkwargs
+        )
+
+        try:
+            r.raise_for_status()
+        except:
+            LOG.debug('%s - exception in request: %s %s',
+                      self.name, r.status_code, r.content)
+            raise
+
+        # parse the table
+        html_soup = BeautifulSoup(r.content, "lxml")
+        table = html_soup.find_all('table')[0]
+        df = pd.read_html(str(table))[0]
+        result = df[df.columns[1]].tolist()
+        result = ' '.join(result).split()
+        return result
+
+    def _process_item(self, item):
+
+        #indicator = item
+        value = {
+            'type': 'IPv4',
+            'confidence': 100
+        }
+
+        return [[item, value]]
+
+
+
